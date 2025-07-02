8e8bdc3cc72b024e5d45026b30ab2d7b72d22f4d
Sagar <ganesha4us1@gmail.com>
2025-07-02 19:26:57 -0400

    favicon update

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/AvantDev-Technologies/public/favicon.ico" />
+        <link rel="icon" href="/public/favicon.ico" />
       </head>
       <body>{children}</body>
     </html>
